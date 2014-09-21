6be88b6dd4102a9dcd0120febc1b1fb35a04e518
Sandeep Supal <sam_supal@yahoo.co.in>
2014-09-21 16:08:54 +0530

    Latest commits

---
+++ paypal.php
@@ -0,0 +1,116 @@
+<?php
+
+?>
+
+
+<form name=”_xclick” action=”https://www.sandbox.paypal.com/webscr” method=”post”>//paypal sandbox post url
+ <input type=”hidden” name=”cmd” value=”_xclick”>
+ <input type=”hidden” name=”business” value=”seller@example.com”>//Paypal sandbox seller account email id
+ <input type=”hidden” name=”currency_code” value=”USD”>//enter your currency code
+ <input type=”hidden” name=”item_name” value=”reesu.org”>//enter the item name
+ <input type=”hidden” name=”return” value=”http://www.example.com/paypal/success.php”>//url to return once payment is done.
+ <input type=”hidden” name=”amount” value=”1″>//amount of transaction needs to be credited to your paypal account
+ <input type=”image” src=”http://www.paypal.com/en_US/i/btn/btn_buynow_LG.gif” border=”0″ name=”submit” alt=”Make payments with PayPal – it’s fast, free and secure!”>
+</form>
+
+
+#081E2A
+#EE2D68
+#F7941E
+
+
+Commented code from Live Classroom page:
+
+	<section id="services">
+		<div class="container">
+			<div class="heading">
+				<!-- Heading -->
+				<h2>What we do</h2>
+				<p>At lorem Ipsum available, but the majority have suffered alteration iien, non gravida purus. Alinon elit imperdiet congue. Integer ultricies sed elit imperdiet congue. Integer ultricieien, non gravida purus. Alinon elit imperdiet congue. Integer ultricies sed elit imperdiet congue. Integer ultricien some form by injected humour.</p>
+				<br />
+			</div>
+			<div class="row">
+
+				<!-- item -->
+				<div class="col-md-4 text-center">
+					<i class="fa fa-arrows fa-2x circle"></i>
+					<h3><span class="id-color">Feature 1</span></h3>
+					<p>Nullam ac rhoncus sapien, non gravida purus. Alinon elit imperdiet congue. Integer elit imperdiet congue.</p>
+				</div>
+				<!-- end: -->
+
+				<!-- item -->
+				<div class="col-md-4 text-center">
+					<i class="fa fa-css3 fa-2x circle"></i>
+					<h3><span class="id-color">Feature 2</span></h3>
+					<p>Nullam ac rhoncus sapien, non gravida purus. Alinon elit imperdiet congue. Integer elit imperdiet congue.</p>
+				</div>
+				<!-- end: -->
+
+				<!-- item -->
+				<div class="col-md-4 text-center">
+					<i class="fa fa-lightbulb-o fa-2x circle"></i>
+					<h3><span class="id-color">Feature 3</span></h3>
+					<p>Nullam ac rhoncus sapien, non gravida purus. Alinon elit imperdiet congue. Integer ultricies sed elit impe.</p>
+				</div>
+				<!-- end: -->
+
+				
+
+			</div>
+		</div>
+		<!--/.container-->
+	</section>
+    
+	<section id="livestream" style="margin:20px auto; width: 660px; display:none;" >
+	 <div id="myElement">
+	 </div>
+	</section>
+	
+	<div id="player" style="border:1px solid #eee; width:800px; height:600px;margin:20px auto;"></div>
+	
+	<footer id="footer">
+		<div class="container">
+			<div class="social text-center">
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-linkedin"></i></a>
+				<a href="#"><i class="fa fa-google-plus"></i></a>
+			</div>
+
+			<div class="clear"></div>
+			<!--CLEAR FLOATS-->
+		</div>
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="simplenav">
+								<a href="index.html">Home</a> | 
+								<a href="about.html">About</a> |
+								<a href="online-courses.html">Courses</a> |
+								<a href="educational-kit.html">Educational Kit</a> |
+								<a href="live-classroom.html">Live Classroom</a> |
+								<a href="fees.html">Fees</a> |
+								<a href="portfolio.html">Our Teachings</a> |
+								<a href="contact.html">Contact</a>
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="text-right">
+								Copyright &copy; 2014. Template by <a href="http://UXIntel.com/" rel="develop">UXIntel.com</a>
+							</p>
+						</div>
+					</div>
+
+				</div>
+				<!-- /row of panels -->
+			</div>
+		</div>
+	</footer>
+
